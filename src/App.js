7cfe93da5d426e3e15bc286563a6604ccdd1f5cc
Filippo <filippo.vallarino@gmail.com>
2022-11-29 11:21:30 +0100

    aggiunto fetch

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import NavMenu from './Components/NavMenu/NavMenu';
 import Header from './Components/Header/Header';
 import MainScreen from './Components/MainScreen/MainScreen';
 import { ScreenStatusContext } from './Context/ScreenStatusContext';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function App() {
 
-  const [screenState, setScreenState] = useState('paginaVuota')
+  const [ screenState, setScreenState ] = useState('paginaVuota')
+  const [ utenti, setUtenti ] = useState('');
+
+  useEffect( () =>{
+    fetch('https://63480ebc0484786c6e90a61b.mockapi.io/Utenti')
+    .then((response) => response.json())
+    .then((json) => setUtenti(json));
+  }, [screenState])
 
   return (
     <div className='page'>
@@ -19,3 +26,4 @@ export default function App() {
   );
 
 }
+

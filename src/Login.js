66c0dc822b83b3eee550f9198911eaf20e3f70b8
Filippo <filippo.vallarino@gmail.com>
2022-12-07 18:17:51 +0100

    prima parte login fatta

--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,51 @@
-import NavMenu from './Components/NavMenu/NavMenu';
-import Header from './Components/Header/Header';
-import MainScreen from './Components/MainScreen/MainScreen'
-import { ScreenStatusContext } from './Context/ScreenStatusContext';
+// import NavMenu from './Components/NavMenu/NavMenu';
+// import Header from './Components/Header/Header';
+// import MainScreen from './Components/MainScreen/MainScreen'
+// import { ScreenStatusContext } from './Context/ScreenStatusContext';
 
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import './login.css'
 
 export default function Login() {
 
-    const [ users, setUsers ] = useState([])
+    const [ usersDB, setUsersDB ] = useState([])
+    const [ nome, setNome ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ error, setError ] = useState(false)
+
     useEffect(() => {
         const fetchData = async () => {
         const result = await axios(
           url
         );
-          setUsers(result.data)
+          setUsersDB(result.data)
         };
           fetchData()
     }, [])
+
+    const handleNomeLogin = (e) => {
+        setNome(e.target.value)
+    }
+
+    const handlerPasswordLogin = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handlerSubmitLogin = (e) => {
+        e.preventDefault()
+        console.log('nome: ', nome, '\npassword: ', password)
+        const daControllare = usersDB.filter( obj => obj.nome === nome )[0]
+        const passwordOriginale = daControllare.password
+        console.log(passwordOriginale)
+        if(password === passwordOriginale) {
+            alert('my man!')
+            setError(false)
+        }else{
+            setError(true)
+        }
+
+    }
 
     const [ screenState, setScreenState ] = useState('formAggiungiDati')
 
@@ -25,15 +53,19 @@ export default function Login() {
 
     return (
         <div className='containerFormLogin'>
-            <form className='formLogin'>        
-                { users ?
-                    <select>
-                        <option defaultValue='scegli..' disabled>Scegli..</option>
-                        {users.map( x => <option key={x.id}>{x.nome}</option>)}
+            <form className='formLogin'> 
+                <div className='titoloLogin'>Accedi</div>
+                { error && <div className='divErrore'>Nome o Password errati!</div> }       
+                { usersDB ?
+                    <select className='selectLogin' defaultValue='scegli' onChange={ handleNomeLogin }>
+                        <option disabled value='scegli'>Scegli..</option>
+                        {usersDB.map( x => <option key={x.id}>{x.nome}</option>)}
                     </select>
                 :
                     'Loading...'
                 }
+                <input type='text' className='passwordLogin' placeholder='password...' onChange={ handlerPasswordLogin } />
+                <input type='submit' className='btnSubmitLogin' onClick={ handlerSubmitLogin } />
             </form>
         </div>
     )

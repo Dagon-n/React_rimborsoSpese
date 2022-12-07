aca01eacae0b27ffa7a5b983c14572234e0dce7b
Filippo <filippo.vallarino@gmail.com>
2022-12-07 14:59:19 +0100

    funziona fetch nella login

--- src/Login.js
+++ src/Login.js
@@ -3,20 +3,49 @@ import Header from './Components/Header/Header';
 import MainScreen from './Components/MainScreen/MainScreen'
 import { ScreenStatusContext } from './Context/ScreenStatusContext';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Login() {
 
+    const [ users, setUsers ] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+        const result = await axios(
+          url
+        );
+          setUsers(result.data)
+        };
+          fetchData()
+    }, [])
+
     const [ screenState, setScreenState ] = useState('formAggiungiDati')
 
+    let url = 'https://63480ebc0484786c6e90a61b.mockapi.io/Utenti';
+
     return (
-        <div className='page'>
-            <NavMenu />
-            <ScreenStatusContext.Provider value={{ screenState, setScreenState }}>
-                <Header />
-                <MainScreen />
-            </ScreenStatusContext.Provider>
+        <div className='containerFormLogin'>
+            <form className='formLogin'>        
+                { users ?
+                    <select>
+                        <option defaultValue='scegli..' disabled>Scegli..</option>
+                        {users.map( x => <option key={x.id}>{x.nome}</option>)}
+                    </select>
+                :
+                    'Loading...'
+                }
+            </form>
         </div>
-    );
+    )
+
+    // return (
+    //     <div className='page'>
+    //         <NavMenu />
+    //         <ScreenStatusContext.Provider value={{ screenState, setScreenState }}>
+    //             <Header />
+    //             <MainScreen />
+    //         </ScreenStatusContext.Provider>
+    //     </div>
+    // );
 
 }
